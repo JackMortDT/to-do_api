b4fe1c61ef873462b9347bd9acbfe4504548f216
Luis 🦎 <luis.6496@hotmail.com>
2023-03-03 12:13:16 -0600

    Adding dependencies for dev and tests and some configurations for use them

--- apps/todo_api/lib/todo_api/mailer.ex
+++ apps/todo_api/lib/todo_api/mailer.ex
@@ -1,3 +1,4 @@
 defmodule TodoApi.Mailer do
+  @moduledoc false
   use Swoosh.Mailer, otp_app: :todo_api
 end
--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule TodoApi.Umbrella.MixProject do
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+      test_coverage: [tool: ExCoveralls],
+      dialyzer: [
+        flags: [:unmatched_returns, :error_handling, :underspecs, :unknown],
+        ignore_warnings: ".dialyzer_ignore.exs",
+        list_unused_filters: true
+      ]
     ]
   end
 
@@ -24,7 +30,17 @@ defmodule TodoApi.Umbrella.MixProject do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps/ folder.
   defp deps do
-    []
+    [
+      # dev deps
+      {:credo, "~> 1.6.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.2.0", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.29.1", only: :dev, runtime: false},
+      {:git_hooks, "~> 0.7.3", only: [:dev], runtime: false},
+      # test deps
+      {:excoveralls, "~> 0.14.5", only: :test},
+      {:ex_machina, "~> 2.7", only: :test},
+      {:faker, "~> 0.17", only: :test}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
@@ -40,6 +56,13 @@ defmodule TodoApi.Umbrella.MixProject do
     [
       # run `mix setup` in all child apps
       setup: ["cmd mix setup"]
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Luis Sastré luis.sastre.dev@gmail.com"],
+      links: %{github: @source_url}
     ]
   end
 end
