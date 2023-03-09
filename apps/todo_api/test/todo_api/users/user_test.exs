6b290516fc745d9feba4a2fa8e9568a7e8d47d60
Luis 🦎 <luis.6496@hotmail.com>
2023-03-08 18:48:40 -0600

    Add user test

---
+++ apps/todo_api/test/todo_api/users/user_test.exs
@@ -0,0 +1,34 @@
+defmodule TodoApi.Users.UserTest do
+  use ExUnit.Case
+
+  alias TodoApi.Users.User
+
+  describe "changeset/2" do
+    test "return valid changeset with valid params" do
+      params = %{
+        username: Faker.Internet.user_name(),
+        email: Faker.Internet.email(),
+        first_name: Faker.Person.first_name(),
+        last_name: Faker.Person.last_name()
+      }
+
+      %Ecto.Changeset{} = changeset = User.changeset(%User{}, params)
+
+      assert changeset.valid?
+    end
+
+    test "fails if not given required fields" do
+      params = %{
+        first_name: Faker.Person.first_name(),
+        email: Faker.Internet.email()
+      }
+
+      %Ecto.Changeset{} = changeset = User.changeset(%User{}, params)
+
+      error = {:username, {"can't be blank", [validation: :required]}}
+      assert error in changeset.errors
+
+      refute changeset.valid?
+    end
+  end
+end
